ca85517802a98b6e60fccd6d40c588965566fb26
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2025-08-25 09:31:48 +0300

    optimize application class

--- src/main/java/io/github/shafthq/SHAFT_MCP/ShaftMcpApplication.java
+++ src/main/java/io/github/shafthq/SHAFT_MCP/ShaftMcpApplication.java
@@ -1,21 +1,13 @@
 package io.github.shafthq.SHAFT_MCP;
 
-import org.apache.commons.io.FileUtils;
 import org.springframework.ai.support.ToolCallbacks;
 import org.springframework.ai.tool.ToolCallback;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Properties;
 
 @SpringBootApplication
 public class ShaftMcpApplication {
@@ -25,17 +17,7 @@ public class ShaftMcpApplication {
      * @param args command-line arguments
      */
 	public static void main(String[] args) throws IOException {
-        SpringApplication app = new SpringApplication(ShaftMcpApplication.class);
-//
-//        URL url = URI.create("https://raw.githubusercontent.com/ShaftHQ/SHAFT_MCP/refs/heads/main/src/main/resources/application.properties").toURL();
-//        File propertiesFile = new File("application.properties");
-//        FileUtils.copyURLToFile(url, propertiesFile);
-//
-//        var props = new Properties();
-//        props.load(Files.newInputStream(Path.of(propertiesFile.toURI())));
-//        app.setBannerMode(Banner.Mode.OFF);
-//        app.setDefaultProperties(props);
-        app.run(args);
+        new SpringApplication(ShaftMcpApplication.class).run(args);
 	}
 
     /**
